65893f37a48f15bf6678b0bea95a03732b33561a
Nick Boeker <nick.boeker@gmail.com>
2017-04-30 16:51:07 -0500

    Continued HW9

--- Homework/9/Question1.py
+++ Homework/9/Question1.py
@@ -1,14 +1,21 @@
 import urllib.request as ur
 from parseTable import parseTable
+import sys
 
 def fetchHTMLTable(link,outputPath):
         ur.urlretrieve(link, filename=outputPath+'exactHTML.html')
-        parsed = parseTable(outputPath+'exactHTML.html')
-        print(parsed)
-        outfile = open(outputPath+'idk.txt','w')
-        outfile.write('test')
+        tableFile = open(outputPath+'exactHTML.html')
+        tableOutput = tableFile.read()
+        parsedHTML = parseTable(tableOutput)
+        outfile = open(outputPath+'resultsTable.out','w')
+        for line in parsedHTML:
+           for item in line:
+              outfile.write('{:>30}'.format(item))
+           outfile.write('\n')
         outfile.close()
+        outfile = open(outputPath+'resultsTable.out','r')
 
 link = "http://butler.lab.asu.edu/swift/bat_time_table.html"
+#21:28
 outputPath = "q1output/"
 fetchHTMLTable(link,outputPath)
